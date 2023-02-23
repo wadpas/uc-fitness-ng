1a8c39c7937e12468a0bec63d12714f4b641cdc7
wadpas <wadpas@gmail.com>
2023-02-23 23:38:24 +0200

    implemented uploading file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import '../assets/css/style.css'
 
 const app = document.getElementById('app')
 app.innerHTML = `<h1>JavaScript HTML5 APIs</h1>
-<div class="uploader">
-    <div id="item-0" class="dragme" draggable="true"></div>
-    <div class="dropzone">Drag to Upload</div>
+<div class="uploader" draggable>
+  <h2>Upload your files</h2>
+  <p>Accepts only .png, .jpg, .svg</p>
+  <div class="dropzone">Drag to Upload</div>
+  <div class="list"></div>
 </div>
 
 <style>
@@ -16,12 +18,6 @@ app.innerHTML = `<h1>JavaScript HTML5 APIs</h1>
     margin: 25px auto;
     padding: 25px;
     background: #fff;
-}
-.dragme {
-    background: #ce1f99;
-    border-radius: 5px;
-    width: 50px;
-    height: 50px;
 }
 .dropzone {
     border-radius: 5px;
@@ -38,32 +34,75 @@ app.innerHTML = `<h1>JavaScript HTML5 APIs</h1>
 `
 
 const init = () => {
-  const dragme = document.querySelector('.dragme')
   const dropzone = document.querySelector('.dropzone')
-
-  dragme.addEventListener('dragstart', (e) =>
-    e.dataTransfer.setData('text/plain', e.target.id)
-  )
+  const list = document.querySelector('.list')
 
   dropzone.addEventListener('dragenter', (e) =>
     e.target.classList.add('active')
   )
+
   dropzone.addEventListener('dragleave', (e) =>
     e.target.classList.remove('active')
   )
+
   dropzone.addEventListener('dragover', (e) => {
     e.preventDefault()
     e.dataTransfer.dropEffect = 'copy'
   })
+
   dropzone.addEventListener('drop', (e) => {
     e.preventDefault()
     e.stopPropagation()
     e.target.classList.remove('active')
-    const id = e.dataTransfer.getData('text/plain')
-    const element = document.getElementById(id)
-    dropzone.append(element)
-    console.log(e.dataTransfer.getData('text/plain'))
+    const { files } = e.dataTransfer
+    handleFileUpload(files)
   })
+
+  const showFilePreview = (file) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.addEventListener('load', (e) => {
+      const div = document.createElement('div')
+      div.innerHTML = `
+      <div style="display: flex">
+        <img
+          src="${e.target.result}"
+          alt="${file.name}"
+          style="width: 120px; margin: 5px">
+          <p>${file.name} <span>${file.size} bytes</span></p>
+      </div>`
+      list.append(div)
+    })
+  }
+
+  const uploadFiles = async (files) => {
+    const form = new FormData()
+
+    ;[...files].forEach((file) => form.append(file.name, file))
+
+    const request = await fetch('//dragdropfiles.glitch.me/upload', {
+      method: 'POST',
+      body: form,
+    })
+
+    return await request.json()
+  }
+
+  const isAllowedType = (file) => {
+    return ['image/png', 'image/jpeg', 'image/svg+xml'].includes(file.type)
+  }
+
+  const handleFileUpload = async (files) => {
+    const filesToUpload = [...files].filter(isAllowedType)
+
+    filesToUpload.forEach(showFilePreview)
+
+    const uploaded = await uploadFiles(filesToUpload)
+
+    if (uploaded) {
+      console.log(uploaded)
+    }
+  }
 
   document.addEventListener('dragover', (e) => e.preventDefault())
   document.addEventListener('drop', (e) => e.preventDefault())
